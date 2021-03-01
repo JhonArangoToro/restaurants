304c128290c026726bea10a4f3d709c1c02f8e1a
jhon.arango.toro <jhon.alejandro.arango.toro@gmail.com>
2021-03-01 16:02:02 -0500

    Initial Push
    Add Navigation

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Navigations from './navigations/Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigations></Navigations>
   );
 }
 
@@ -18,4 +16,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
